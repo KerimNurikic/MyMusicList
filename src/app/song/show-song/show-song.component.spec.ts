a94f48c7d3d9873b195c53293259dbf646a21367
KerimNurikicETF <kerimnurikic16@gmail.com>
2022-02-11 03:42:04 +0100

    final

---
+++ src/app/song/show-song/show-song.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowSongComponent } from './show-song.component';
+
+describe('ShowSongComponent', () => {
+  let component: ShowSongComponent;
+  let fixture: ComponentFixture<ShowSongComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShowSongComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowSongComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/song/show-song/show-song.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SongApiService } from 'src/app/song-api.service';
+
+@Component({
+  selector: 'app-show-song',
+  templateUrl: './show-song.component.html',
+  styleUrls: ['./show-song.component.css'],
+})
+export class ShowSongComponent implements OnInit {
+  songList$!: Observable<any[]>;
+  genresList$!: Observable<any[]>;
+  genresList: any = [];
+
+  genresMap: Map<number, string> = new Map();
+
+  constructor(private service: SongApiService) {}
+
+  ngOnInit(): void {
+    this.songList$ = this.service.getSongList();
+    this.refreshGenresMap();
+  }
+
+  //Variables
+  modalTitle: string = '';
+  activateAddEditSongComponent: boolean = false;
+  song: any;
+
+  modalAdd() {
+    this.song = {
+      id: 0,
+      title: null,
+      artist: null,
+      url: null,
+      rating: 3,
+      favorite: null,
+      dateAdded: null,
+      dateEdited: null,
+      genreId: null,
+    };
+    this.modalTitle = 'Add Song';
+    this.activateAddEditSongComponent = true;
+  }
+
+  modalEdit(item:any){
+    this.song=item;
+    this.modalTitle = 'Edit Song';
+    this.activateAddEditSongComponent = true;
+  }
+
+  delete(item:any){
+    if(confirm(`Are you sure you want to delete "${item.title}" by ${item.artist}?`)){
+      this.service.deleteSong(item.id).subscribe(res=>{
+      var closeModalBtn = document.getElementById('add-edit-modal-close');
+      if(closeModalBtn){
+        closeModalBtn.click();
+      }
+
+      var showDeleteSuccess = document.getElementById('delete-success-alert');
+      if(showDeleteSuccess){
+        showDeleteSuccess.style.display="block";
+      }
+      setTimeout(function(){
+        if(showDeleteSuccess){
+          showDeleteSuccess.style.display="none";
+        }
+      },4000);
+      this.songList$=this.service.getSongList();
+      })
+    }
+  }
+
+  modalClose() {
+    this.activateAddEditSongComponent = false;
+    this.songList$ = this.service.getSongList();
+  }
+
+  refreshGenresMap() {
+    this.service.getGenresList().subscribe((data) => {
+      this.genresList = data;
+
+      for (let i = 0; i < data.length; i++) {
+        this.genresMap.set(this.genresList[i].id, this.genresList[i].name);
+      }
+    });
+  }
+}
